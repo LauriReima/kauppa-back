37d8d8b4941be3609fd5ca8efd4a587e03254abf
Lauri Reima <laurireima@LaRe-MacBook-Pro.local>
2023-02-22 18:11:31 +0200

    get-funktio

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain'})
-    res.end('Hello World')
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only JavaScript",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      important: true
+    }
+  ]
+
+app.get('/', (req,res) => {
+    res.send('<h1>Hello world</h1>')
+})
+
+app.get('/api/notes', (req,res) => {
+    res.json(notes)
+})
+
+app.get('/api/notes/:id', (req,res) => {
+    const id = Number(req.params.id)
+    const note = notes.find(note => note.id === id)
+
+    if (note) {
+        res.json(note)
+    }
+    else {
+        res.status(404).end()
+    }
+    
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+   console.log(`Server running on port ${PORT}`); 
+})
