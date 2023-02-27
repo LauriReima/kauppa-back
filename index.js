a6c7f9c65cdb57530dd53c67f476dd079abd8f5b
Lauri Reima <laurireima@dyn224-165.eduroam.net.utu.fi>
2023-02-27 18:40:24 +0200

    jotain

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
+app.use(cors())
 app.use(express.json())
-
+app.use(express.static('build'))
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -86,7 +89,7 @@ app.delete('/api/notes/:id', (req,res) => {
     res.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
    console.log(`Server running on port ${PORT}`); 
 })
